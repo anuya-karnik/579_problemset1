2ca7c107b6fac304881320aa4af10bcb48025922
Anuya Karnik <aakarnik8@gmail.com>
2021-02-10 16:44:32 -0500

    ex. 2 complete

--- ps3-myJS.js
+++ ps3-myJS.js
@@ -11,13 +11,11 @@ function changeColor() {
         mbox.setAttribute('id', 'color-block-change');
         console.log("color changed to green from red");
         mtext.textContent = "#08F080";
-        // mbox = document.getElementById("color-block-change");        
     }
     else {
         mbox.setAttribute('id', 'color-block');
         console.log("color changed to red from green");
         mtext.textContent = "#F08080";
-        // mbox = document.getElementById('color-block');
     }
 }
 
@@ -28,10 +26,21 @@ function changeColor() {
 * Then write a function that calculates Fahrenheit to Celsius and display it on the webpage
 */
 
+let tempconvert = document.getElementById('convertbtn');
+let finput = document.getElementById('f-input');
+let cresult = document.getElementById('c-output');
+
+tempconvert.addEventListener('click', convertTemp);
+
 function convertTemp(){
     //Calculate the temperature here
+    ftemp = finput.value;
+    console.log("The temperature in Fahrenheit is ", ftemp);
+    let result = (ftemp - 32) * 5/9;
+    console.log("The converted temperature is ", result);
 
     //Send the calculated temperature to HTML
+    cresult.textContent = result.toPrecision(4);
 
 }
 
